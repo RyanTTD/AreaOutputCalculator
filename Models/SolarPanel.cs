512915a9e83d083eea13d845fb1a1fe80b7ce5f0
Ryan-IU <ryan@ingenuityunlimited.co.za>
2024-01-30 10:30:11 +0200

    Initial

---
+++ Models/SolarPanel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOutputCalculator.Models
+{
+    internal class SolarPanel
+    {
+        public SolarPanel(int length, int width, int outputPerUnitArea)
+        {
+            Length = length;
+            Width = width;
+            OutputPerUnitArea = outputPerUnitArea;
+        }
+
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int PanelArea 
+        { 
+            get
+            {
+                return Length * Width;
+            }
+        }
+        public int OutputPerUnitArea { get; set; }
+        public int TotalUnitOutput 
+        { 
+            get
+            {
+                return OutputPerUnitArea * PanelArea;
+            }
+        }
+
+        public decimal UnitOutputToAreaRatio
+        {
+            get
+            {
+                if (PanelArea == 0) return 0;
+
+                return OutputPerUnitArea / PanelArea;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using AreaOutputCalculator.Models;
+
+SolarPanel[] panels = new[]
+{
+    new SolarPanel(3, 3, 10),
+    new SolarPanel(2, 7, 12),
+    new SolarPanel(3, 4, 14),
+};
+
+var landArea = 81;
+
+Console.Write(MaxOutput(panels, landArea));
+
+int MaxOutput(SolarPanel[] panels, int landArea)
+{
+    var result = 0;
+    int tempResult = 0;
+
+    // order the panels to get the "biggest area" first
+    panels = panels.OrderByDescending(o => o.PanelArea).ToArray();
+    result = GetPanelsTotal(panels, landArea);
+
+    // order the panels to get the "highest OutputPerUnitArea" first
+    panels = panels.OrderByDescending(o => o.OutputPerUnitArea).ToArray();
+    tempResult = GetPanelsTotal(panels, landArea);
+    if (tempResult > result) result = tempResult;
+
+    // order the panels to get the "highest OutputPerUnitArea by area" first
+    panels = panels.OrderByDescending(o => (o.TotalUnitOutput)).ToArray();
+    tempResult = GetPanelsTotal(panels, landArea);
+    if (tempResult > result) result = tempResult;
+
+    // order the panels to get the "highest ratio of OutputPerUnitArea vs area" first
+    panels = panels.OrderByDescending(o => o.UnitOutputToAreaRatio).ToArray();
+    tempResult = GetPanelsTotal(panels, landArea);
+    if (tempResult > result) result = tempResult;
+
+    return result;
+}
+
+int GetPanelsTotal(SolarPanel[] panels, int landArea)
+{
+    var result = 0;
+
+    foreach (var panel in panels)
+    {
+        while (landArea > panel.PanelArea)
+        {
+            landArea -= panel.PanelArea;
+            result += panel.TotalUnitOutput;
+        }
+    }
+
+    return result;
+}
