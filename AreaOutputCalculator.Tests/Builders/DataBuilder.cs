471bc2e91751dbd8fd18c9c5869f3b0fd1e6398b
RyanTTD <149164666+RyanTTD@users.noreply.github.com>
2024-01-30 14:35:58 +0200

    Project re-structure

---
+++ AreaOutputCalculator.Tests/Builders/DataBuilder.cs
@@ -0,0 +1,72 @@
+﻿using AreaOutputCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOutputCalculator.Tests.Builders
+{
+    public class DataBuilder
+    {
+        public SolarPanel[] BuildWithWorkingData()
+        {
+            SolarPanel[] panels = new[]
+            {
+                new SolarPanel(2, 5, 12),
+                new SolarPanel(4, 12, 20),
+                new SolarPanel(5, 5, 25),
+            };
+
+            return panels;
+        }
+
+        public SolarPanel[] BuildWithMoreWorkingData()
+        {
+            SolarPanel[] panels = new[]
+            {
+                new SolarPanel(3, 2, 8),
+                new SolarPanel(6, 10, 20),
+                new SolarPanel(9, 5, 15),
+            };
+
+            return panels;
+        }
+
+        public SolarPanel[] BuildWithLastWorkingData()
+        {
+            SolarPanel[] panels = new[]
+            {
+                new SolarPanel(5, 2, 8),
+                new SolarPanel(2, 10, 12),
+                new SolarPanel(7, 5, 20),
+            };
+
+            return panels;
+        }
+
+        public SolarPanel[] BuildWithNoData()
+        {
+            SolarPanel[] panels = new SolarPanel[0];
+
+            return panels;
+        }
+
+        public SolarPanel[] BuildWithLotsOfData()
+        {
+            SolarPanel[] panels = new[]
+            {
+                new SolarPanel(2, 9, 12),
+                new SolarPanel(3, 8, 14),
+                new SolarPanel(4, 7, 16),
+                new SolarPanel(5, 6, 17),
+                new SolarPanel(6, 5, 20),
+                new SolarPanel(7, 5, 25),
+                new SolarPanel(8, 4, 19),
+                new SolarPanel(9, 4, 21)
+            };
+
+            return panels;
+        }
+    }
+}
---
+++ AreaOutputCalculator.Tests/Classes/AreaCalculatorHelperTests.cs
@@ -0,0 +1,148 @@
+﻿using AreaOutputCalculator.Helpers;
+using AreaOutputCalculator.Tests.Builders;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOutputCalculator.Tests.Classes
+{
+    [TestFixture]
+    public class AreaCalculatorHelperTests
+    {
+        [TestFixture]
+        public class MaxOutput
+        {
+            [Test]
+            public void Given_ValidData_Must_ReturnValueGreaterThanZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithWorkingData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().BeGreaterThan(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_ValidData_And_NegativeArea_Must_ReturnZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = -93;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithWorkingData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().Be(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_MoreValidData_Must_ReturnValueGreaterThanZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithMoreWorkingData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().BeGreaterThan(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_LastValidData_Must_ReturnValueGreaterThanZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithLastWorkingData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().BeGreaterThan(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_ValidData_Must_ReturnExactValue()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithWorkingData();
+
+                var expectedOutcome = 2115;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().BeGreaterThanOrEqualTo(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_ValidData_And_LowLandArea_Must_ReturnZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 2;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithWorkingData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().Be(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_NoData_Must_ReturnZero()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithNoData();
+
+                var expectedOutcome = 0;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().Be(expectedOutcome);
+            }
+
+            [Test]
+            public void Given_LotsOfData_Must_ReturnExactValue()
+            {
+                var areaCalculatorHelperToTest = new AreaCalculatorHelper();
+
+                var solarPanelArea = 100;
+                var solarPanelsToInput = new DataBuilder()
+                    .BuildWithLotsOfData();
+
+                var expectedOutcome = 2198;
+
+                var actualOutcome = areaCalculatorHelperToTest.MaxOutput(solarPanelsToInput, solarPanelArea);
+
+                actualOutcome.Should().BeGreaterThanOrEqualTo(expectedOutcome);
+            }
+        }
+    }
+}
